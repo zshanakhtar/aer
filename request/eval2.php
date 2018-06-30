4e54c1ff64680f060112506afc1a0849a34f8837
Zeeshan <zshanakhtar@oulook.com>
2018-07-01 03:20:13 +0530

    too minor

--- request/eval2.php
+++ request/eval2.php
@@ -14,7 +14,7 @@ if($row = $resultsum->fetch_assoc())
 	{
 	$query ="UPDATE judge set abstract_score='$abstract_score',Q1_score='$Q1_score', Q2_score ='$Q2_score', Q3_score ='$Q3_score', Q4_score ='$Q4_score', Q5_score ='$Q5_score', flageval2='Y' WHERE app_id='$app_id'";//, flageval2='$flageval2'
 	mysqli_query($conn, $query);
-    echo "Marks Saved, switching to next application";
+    echo "Scores Saved, switching to next application";
     $resultapp=mysqli_query($conn,"SELECT * FROM judge WHERE judge_id='$username' AND flageval2='N'");
     $rowapp = $resultapp->fetch_assoc();
     $app_id=$rowapp['app_id'];
