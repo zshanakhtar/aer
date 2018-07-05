c980b2ded9bc1dd09b09bd625ad56afc4215e310
Zeeshan <zshanakhtar@oulook.com>
2018-07-05 16:44:00 +0530

    navigation for user creation provided on adash

--- section/navadmin.php
+++ section/navadmin.php
@@ -12,12 +12,12 @@
 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
   <ul class="nav navbar-nav">
-<li class="active"><a href="#page1" data-module="summary">Summary</a></li>
-<li><a href="#page2" data-module="view_app">View Applications</a></li>
-<li><a href="#page3" data-module="create_judge">Create Judges</a></li>
-<li><a href="#page4" data-module="allocate_judge">Allocate Judges</a></li>
-<li><a href="#page5" data-module="under">Under Cons</a></li>
-<li><a href="#page6" data-module="under">Under Cons</a></li>
+<li class="active"><a href="javascript:void(0)" data-module="summary">Summary</a></li>
+<li><a href="javascript:void(0)" data-module="view_app">View Applications</a></li>
+<li><a href="javascript:void(0)" data-module="create_judge">Create Judges</a></li>
+<li><a href="javascript:void(0)" data-module="create_user">Create User</a></li>
+<li><a href="javascript:void(0)" data-module="allocate_judge">Allocate Judges</a></li>
+<li><a href="javascript:void(0)" data-module="under">Under Cons</a></li>
   </ul>
 </div>
 </div>
