40b41d0636b388309904019cddc2e17053922771
Zeeshan <zshanakhtar@oulook.com>
2018-07-06 07:17:03 +0530

    minor fixes -  code removal, adash

--- section/adashboard.php
+++ section/adashboard.php
@@ -3,12 +3,12 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 
-<title>Administrator Dashboard</title>
+<title>Administrator Dashboard - See all application forms, Create new users, Allot manager</title>
 
 <!-- Bootstrap -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   
 <!-- Validator   -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.min.js"></script> 
@@ -29,35 +29,19 @@
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs/jszip-2.5.0/dt-1.10.16/b-1.5.1/b-flash-1.5.1/b-html5-1.5.1/datatables.min.js"></script>
   
 </head>
-<?php
-include "request/"."connection.php";
-
-extract($_SESSION);
-$app_id=trim($username);
-$query="select * from regist where username='$app_id'";
-$result=mysqli_query($conn,$query);
- $app_id2=mysqli_fetch_array($result,MYSQLI_ASSOC);
-
-$query="select * from teammember where app_id='$app_id'";
-$resultteam=mysqli_query($conn,$query);
- 
-?>
 <body>
 
 <?php include 'section/ajaxloader.php'; ?>
 <?php include 'section/navdashboard.php'; ?>
-<?php include 'section/navadmin.php'; ?>
+<?php include 'section/nav_a.php'; ?>
 
-
- <img src="images/logo.png"  style="margin-top:10px;margin-left:150px;width:25%;" alt="Image">
- <br>
- 
- 
+<img src="images/logo.png"  style="margin-top:10px;margin-left:150px;width:25%;" alt="Image">
+<br>
 <div id="myDiv1"></div>
 <br>
 <div class="review col-xs-12 col-sm-10 col-sm-offset-1">
-
 </div>
+
 <?php include 'section/footer.php'; ?>
 </body>
 </html>
