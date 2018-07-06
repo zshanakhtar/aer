2a5bc486d451bfcdaa371cefb5499a6865989fc7
Zeeshan <zshanakhtar@oulook.com>
2018-07-06 07:20:25 +0530

    minor fixes- removed extra code from jdash

--- section/jdashboard.php
+++ section/jdashboard.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Judge Dashboard- View Applications, Evaluate and more</title>
+<title>Judge - Scoring Applications, Preview Scores</title>
 
 <!-- Bootstrap -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -28,32 +28,14 @@
 
 <?php include 'section/ajaxloader.php'; ?>
 <?php include "navdashboard.php"; ?>
-<?php include "navjudge.php"; ?>
+<?php include "nav_j.php"; ?>
+
+<img src="images/logo.png"  style="margin-top:10px;margin-left:150px;width:25%;" alt="Image">
 <br>
-  <div id="myDiv1"></div>
-<div  class="review col-xs-12 col-sm-10 col-sm-offset-1">
-
-
-
+<div id="myDiv1"></div>
+<br>
+<div class="review col-xs-12 col-sm-10 col-sm-offset-1">
 </div>
-
-<script>
-function changeview(elem){
-$.ajax({
-    url: "request/getmodule.php",
-    type: "POST",
-    data: "module="+$(elem).data('module'),
-    success: function(response){ 
-        $('.review').html(response);
-        //handle returned arrayList
-    },
-    error: function(e){  
-        alert("error");
-        //handle error
-    } 
-});
-}
-</script>
 
 <?php include 'section/footer.php'; ?>
 </body>
