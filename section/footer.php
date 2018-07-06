9cf53892863fafeb53299487730a9b77d1aa1589
Zeeshan <zshanakhtar@oulook.com>
2018-07-06 16:26:54 +0530

    index page

--- section/footer.php
+++ section/footer.php
@@ -2,8 +2,36 @@
 <!-- <div class="col-xs-12 text-center" style="height:200px;"> -->
 
 <!-- </div> -->
+<div class="container">
+<marquee>
+<div class="col-xs-12">
+<h2 class="text-danger">*Do not use Internet Explore to Fill the Entry*</h2>
+</div>
+</marquee>
+</div>
 <footer class="col-xs-12 text-center">
-  <p>All India Council of Technical Education</p>
+
+
+
+  <div class="col-xs-3">
+  <img src="images/m.jpg" class="img-responsive" width="60%">
+  
+  </div>
+  
+  <div class="col-xs-3">
+  <img src="images/n.png" class="img-responsive" width="30%">
+  
+  </div>
+
+
+<div class="col-xs-6 pull-right">
+ 
+      <p><span class="glyphicon glyphicon-map-marker"></span>New delhi</p>
+      <p><span class="glyphicon glyphicon-phone"></span> 01129581322</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> sdc@aicte-india.org</p> 
+  </div>
+  
+
 </footer>
 
 
