219ac4f0335fde0e15d5274470216b17e659f518
Zeeshan <zshanakhtar@oulook.com>
2018-07-20 23:10:51 +0530

    panel added on problem module

--- section/a_view_app.php
+++ section/a_view_app.php
@@ -1,31 +1,29 @@
-<ul class="nav nav-tabs" id="problem_tabs">
-      <li class="active"><a data-toggle="tab" data-i="0">Problem 0&nbsp;<span class="btn btn-xs btn-danger" id="pr0_count"></span></a></li>
-      <li><a data-toggle="tab" data-i="1">Problem 1&nbsp;<span class="btn btn-xs btn-danger" id="pr1_count"></span></a></li>
-      <li><a data-toggle="tab" data-i="2">Problem 2&nbsp;<span class="btn btn-xs btn-danger" id="pr2_count"></span></a></li>
-      <li><a data-toggle="tab" data-i="3">Problem 3&nbsp;<span class="btn btn-xs btn-danger" id="pr3_count"></span></a></li>
-      <li><a data-toggle="tab" data-i="4">Problem 4&nbsp;<span class="btn btn-xs btn-danger" id="pr4_count"></span></a></li>
-      <li><a data-toggle="tab" data-i="5">Problem 5&nbsp;<span class="btn btn-xs btn-danger" id="pr5_count"></span></a></li>
-      <li><a data-toggle="tab" data-i="6">Problem 6&nbsp;<span class="btn btn-xs btn-danger" id="pr6_count"></span></a></li>
-      <li><a data-toggle="tab" data-i="7">Problem 7&nbsp;<span class="btn btn-xs btn-danger" id="pr7_count"></span></a></li>
-  </ul>
-  <div class="tab-content">
-      <div class="subreview">
+<div class="panel panel-danger">
+    <div class="panel-heading">Applications</div>
+    <div class="panel-body">
+        <ul class="nav nav-tabs" id="problem_tabs">
+            <li class="active"><a data-toggle="tab" data-i="0">Problem 0&nbsp;<span class="btn btn-xs btn-danger" id="pr0_count"></span></a></li>
+            <li><a data-toggle="tab" data-i="1">Problem 1&nbsp;<span class="btn btn-xs btn-danger" id="pr1_count"></span></a></li>
+            <li><a data-toggle="tab" data-i="2">Problem 2&nbsp;<span class="btn btn-xs btn-danger" id="pr2_count"></span></a></li>
+            <li><a data-toggle="tab" data-i="3">Problem 3&nbsp;<span class="btn btn-xs btn-danger" id="pr3_count"></span></a></li>
+            <li><a data-toggle="tab" data-i="4">Problem 4&nbsp;<span class="btn btn-xs btn-danger" id="pr4_count"></span></a></li>
+            <li><a data-toggle="tab" data-i="5">Problem 5&nbsp;<span class="btn btn-xs btn-danger" id="pr5_count"></span></a></li>
+            <li><a data-toggle="tab" data-i="6">Problem 6&nbsp;<span class="btn btn-xs btn-danger" id="pr6_count"></span></a></li>
+            <li><a data-toggle="tab" data-i="7">Problem 7&nbsp;<span class="btn btn-xs btn-danger" id="pr7_count"></span></a></li>
+        </ul>
+        <div class="tab-content subreview">
 
-      </div>
-      
-          <div class="panel panel-danger">
-        	<div class="panel-heading" data-toggle="collapse" data-target="#playground" style="font-size:150%;"><b>Playground</b><span class="btn btn-danger pull-right glyphicon glyphicon-chevron-up"></span></div>
-        	<div  class="panel-body collapse in viewsegue" id="playground">
-        		<h1 style="margin-left:100px;">
-        		Admin Playground
-        		</h1>
-        	</div>
         </div>
-      
-  </div>
-
-
-
+    </div>
+</div>
+<div class="panel panel-danger">
+    <div class="panel-heading" data-toggle="collapse" data-target="#playground" style="font-size:150%;"><b>Playground</b><span class="btn btn-danger pull-right glyphicon glyphicon-chevron-up"></span></div>
+    <div  class="panel-body collapse in viewsegue" id="playground">
+        <h1 style="margin-left:100px;">
+            Admin Playground
+        </h1>
+    </div>
+</div>
 
   <script>
   $(document).ready(function(){
