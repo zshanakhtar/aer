1bb5df7247cdde30cf63cd25c9bc6a51c76b1b04
Zeeshan <zshanakhtar@oulook.com>
2018-07-21 00:16:10 +0530

    added evaluation flags in insight segue

--- section/a_s_insight.php
+++ section/a_s_insight.php
@@ -4,11 +4,51 @@ include "../request/"."connection.php";
 $resultsum=mysqli_query($conn,"SELECT * FROM student WHERE app_id='$app_id'");
 $row = $resultsum->fetch_assoc();
 
+$resulteval1=mysqli_query($conn,"SELECT count(*) FROM evaluator WHERE app_id='$app_id'");
+$roweval1 = $resulteval1->fetch_assoc();
+$resulteval1N=mysqli_query($conn,"SELECT count(*) FROM evaluator WHERE app_id='$app_id' AND flageval1='N'");
+$roweval1N = $resulteval1N->fetch_assoc();
+if($roweval1N['count(*)']==0)
+{
+    $flageval1='Y';
+}
+else if($roweval1['count(*)']==$roweval1N['count(*)'])
+{
+    $flageval1='N';
+}
+else
+{
+    $flageval1='P';
+}
+
+$resulteval2=mysqli_query($conn,"SELECT count(*) FROM judge WHERE app_id='$app_id'");
+$roweval2 = $resulteval2->fetch_assoc();
+$resulteval2N=mysqli_query($conn,"SELECT count(*) FROM judge WHERE app_id='$app_id' AND flageval2='N'");
+$roweval2N = $resulteval2N->fetch_assoc();
+if($roweval2N['count(*)']==0)
+{
+    $flageval2='Y';
+}
+else if($roweval2['count(*)']==$roweval2N['count(*)'])
+{
+    $flageval2='N';
+}
+else
+{
+    $flageval2='P';
+}
 ?>
 
 <div class="panel panel-info">
 	<div class="panel-heading" data-toggle="collapse" data-target="#four" style="font-size:150%;"><b>Student form insights</b><span class="btn btn-info pull-right glyphicon glyphicon-chevron-up"></span></div>
 	<div  class="panel-body collapse in one" id="four">
+        <div class="col-sm-12 form-group">
+			<label for="app_id" class="col-sm-2 control-label" style="color:#337ab7; font-size:14px">Application Number</label>
+            <div class="col-sm-10">
+                <input id="app_id" name="app_id" type="text" class="form-control" disabled required />
+			</div>
+		</div>
+
 		<div class="col-sm-4 form-group">
 			<label for="flagsec1" class="col-sm-5 control-label" style="color:#337ab7; font-size:14px">General Information (Sec 1)</label>
             <div class="col-sm-3">
@@ -75,28 +115,61 @@ $row = $resultsum->fetch_assoc();
             </div>
 		</div>
 		
-		<div class="col-sm-4 form-group">
-			<label for="app_status" class="col-sm-5 control-label" style="color:#337ab7; font-size:14px">Application Status</label>
-            <div class="col-sm-3">
+		<div class="col-sm-6 form-group">
+			<label for="app_status" class="col-sm-4 control-label" style="color:#337ab7; font-size:14px">Application Status</label>
+            <div class="col-sm-5">
                 <input id="app_status" name="app_status" type="text" class="form-control" />
 			</div>
-            <div class="scorer col-sm-4" data-qname="app_status">
+            <div class="scorer col-sm-3" data-qname="app_status">
                 <i class="btn btn-default" data-eval="Submitted"><span class="glyphicon glyphicon-ok"></span></i>
                 <i class="btn btn-default" data-eval=""><span class="glyphicon glyphicon-remove"></span></i>
             </div>
 		</div>
 		
-		
+		<div class="col-sm-3 form-group">
+			<label for="flageval1" class="col-sm-4 control-label" style="color:#337ab7; font-size:14px">Evaluated</label>
+            <div class="">
+                <input id="flageval1" type="text" class="form-control hidden" />
+			</div>
+            <div class="scorer-disabled col-sm-8" data-qname="flageval1">
+                <i class="btn btn-default" data-eval="N"><span class="glyphicon glyphicon-remove"></span></i>
+                <i class="btn btn-default" data-eval="P"><span class="glyphicon glyphicon-time"></span></i>
+                <i class="btn btn-default" data-eval="Y"><span class="glyphicon glyphicon-ok"></span></i>
+            </div>
+		</div>
+
+        <div class="col-sm-3 form-group">
+			<label for="flageval2" class="col-sm-4 control-label" style="color:#337ab7; font-size:14px">Scored</label>
+            <div class="">
+                <input id="flageval2" type="text" class="form-control hidden" />
+			</div>
+            <div class="scorer-disabled col-sm-8" data-qname="flageval2">
+                <i class="btn btn-default" data-eval="N"><span class="glyphicon glyphicon-remove"></span></i>
+                <i class="btn btn-default" data-eval="P"><span class="glyphicon glyphicon-time"></span></i>
+                <i class="btn btn-default" data-eval="Y"><span class="glyphicon glyphicon-ok"></span></i>
+            </div>
+		</div>
+
 	</div>
 </div>
 
 <script>
-    $('#flagsec1,#flagsec2,#flagsec3,#flagsec4,#flagsec5,#flagsec6,#app_status').on('keyup',function(e){
+    $('#flagsec1,#flagsec2,#flagsec3,#flagsec4,#flagsec5,#flagsec6,#app_status,#flageval1,#flageval2').on('keyup',function(e){
         var score=$(this).val().toUpperCase();
-        $(this).closest('.form-group').find('.scorer i').each(function(){
+        $(this).closest('div.form-group').find('.scorer i').each(function(){
             if($(this).data('eval').toUpperCase()==score)
             {
                 $(this).click();
+            }
+        });
+
+        $(this).closest('div.form-group').find('.scorer-disabled i').each(function(){
+            if($(this).data('eval').toUpperCase()==score)
+            {
+                $(this).addClass("btn-warning");
+                $(this).removeClass("btn-default");
+                $(this).siblings().removeClass("btn-warning");
+                $(this).siblings().addClass("btn-default");
             }
         });
     });
@@ -111,6 +184,7 @@ $row = $resultsum->fetch_assoc();
         $('#'+str).val(score);
     });
 
+    $("#app_id").val("<?php echo addslashes($app_id);?>");
     $("#flagsec1").val("<?php echo addslashes($row['flagsec1']);?>");
     $('#flagsec1').trigger('keyup');
     $("#flagsec2").val("<?php echo addslashes($row['flagsec2']);?>");
@@ -125,5 +199,9 @@ $row = $resultsum->fetch_assoc();
     $('#flagsec6').trigger('keyup');
     $("#app_status").val("<?php echo addslashes($row['app_status']);?>");
     $('#app_status').trigger('keyup');
+    $("#flageval1").val("<?php echo addslashes($flageval1);?>");
+    $('#flageval1').trigger('keyup');
+    $("#flageval2").val("<?php echo addslashes($flageval2);?>");
+    $('#flageval2').trigger('keyup');
     
 </script>
--- section/template.php
+++ section/template.php
@@ -1,41 +1,53 @@
-<ul class="nav nav-tabs">
-    <li class="active"><a data-toggle="tab" href="#problem1">Problem 1</a></li>
-    <li><a data-toggle="tab" href="#problem2">Problem 2</a></li>
-    <li><a data-toggle="tab" href="#problem3">Problem 3</a></li>
-    <li><a data-toggle="tab" href="#problem4">Problem 4</a></li>
-    <li><a data-toggle="tab" href="#problem5">Problem 5</a></li>
-    <li><a data-toggle="tab" href="#problem6">Problem 6</a></li>
-    <li><a data-toggle="tab" href="#problem7">Problem 7</a></li>
+<style>
     
-  </ul>
+</style>
 
-  <div class="tab-content">
-    <div id="problem1" class="tab-pane fade in active">
-      <h3>Problem 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-	<div id="problem2" class="tab-pane fade">
-      <h3>Problem 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-	<div id="problem3" class="tab-pane fade">
-      <h3>Problem 3</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-	<div id="problem4" class="tab-pane fade">
-      <h3>Problem 4</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-	<div id="problem5" class="tab-pane fade">
-      <h3>Problem 5</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-	<div id="problem6" class="tab-pane fade">
-      <h3>Problem 6</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-	<div id="problem7" class="tab-pane fade">
-      <h3>Problem 7</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-  </div>
+<div class="panel panel-info">
+	<div class="panel-heading" data-toggle="collapse" data-target="#four" style="font-size:150%;"><b>Student form insights</b><span class="btn btn-info pull-right glyphicon glyphicon-chevron-up"></span></div>
+	<div  class="panel-body collapse in one" id="four">
+		<div class="row form-group">
+			<label for="flagsec1" class="col-sm-5 control-label" style="color:#337ab7; font-size:14px">General Information (Sec 1)</label>
+            <div class="col-sm-2">
+                
+			</div>
+			<div class="col-sm-1">
+				<input id="flagsec1" name="flagsec1" type="text" class="form-control" required />
+			</div>
+		</div>
+		<div class="row form-group">
+			<label for="stream" class="col-sm-2 control-label" style="color:#337ab7; font-size:14px">(Sec 2)</label>
+			<div class="col-sm-10">
+				<input id="stream" name="stream" type="text" class="form-control" required />
+			</div>
+		</div>
+		<div class="row form-group">
+			<label for="qualification" class="col-sm-2 control-label" style="color:#337ab7; font-size:14px">(Sec 3)</label>
+			<div class="col-sm-10">
+				<input id="qualification" name="qualification" type="text" class="form-control" required />
+			</div>
+		</div>
+		<div class="row form-group">
+			<label for="mentor_desi" class="col-sm-2 control-label" style="color:#337ab7; font-size:14px">(Sec 4)</label>
+			<div class="col-sm-10">
+				<input id="mentor_desi" name="mentor_desi" type="text" class="form-control" required />
+			</div>
+		</div>
+		<div class="row form-group">
+			<label for="mentor_email" class="col-sm-2 control-label" style="color:#337ab7; font-size:14px">(Sec 5)</label>
+			<div class="col-sm-10">
+				<input id="mentor_email" name="mentor_email" type="email" class="form-control" required />
+			</div>
+		</div>
+		<div class="row form-group">
+			<label for="mentor_mobile" class="col-sm-2 control-label" style="color:#337ab7; font-size:14px">(Sec 6)</label>
+			<div class="col-sm-10">
+				<input id="mentor_mobile" name="mentor_mobile" type="text" pattern='[6-9]{1}[0-9]{9}' class="form-control" required />
+			</div>
+		</div>
+		
+	</div>
+</div>
+
+<script>
+
+</script>
