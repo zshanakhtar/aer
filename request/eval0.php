1fa788f8e07c446efb78d8fabbcba7ad25dd3476
Zeeshan <zshanakhtar@oulook.com>
2018-09-06 23:31:14 +0530

    add score det to mdash

--- request/eval0.php
+++ request/eval0.php
@@ -9,20 +9,30 @@ extract($_SESSION);
 
 // echo $username;
 
-$resultsum=mysqli_query($conn,"SELECT * FROM manager WHERE manager_id='$username' AND app_id='$app_id'");
-if($row = $resultsum->fetch_assoc() && $usertype=='m')
+    $resultsum=mysqli_query($conn,"SELECT * FROM manager WHERE manager_id='$username' AND app_id='$app_id'");
+    if($row = $resultsum->fetch_assoc() && $usertype=='m')
 	{
-	$query ="UPDATE manager set zero_score='$zero_score', flageval0='Y' WHERE app_id='$app_id'and manager_id='$username'";
-	mysqli_query($conn, $query);
-    echo "Successfully updated";
-    // $resultapp=mysqli_query($conn,"SELECT * FROM judge WHERE judge_id='$username' AND flageval2='N'");
-    // $rowapp = $resultapp->fetch_assoc();
-    // $app_id=$rowapp['app_id'];
-    // $_SESSION['app_id']=$app_id;
-   }
-   else
-   {
-	   echo "Server Error";
-   }
+        $result2=mysqli_query($conn,"SELECT * FROM manager WHERE manager_id='$username' AND app_id='$app_id'");
+        $row2 = $result2->fetch_assoc();
+        // echo $row2['zero_score'];
+        if($row2['flageval0']=='Y')
+            echo "Application already verified, unable to change";
+        else if($zero_score==0 && $zero_det==NULL)
+            echo "Please indicate reason for rejection";
+        else
+        {
+            $query ="UPDATE manager set zero_score='$zero_score',zero_det='$zero_det', flageval0='Y' WHERE app_id='$app_id'and manager_id='$username'";
+            mysqli_query($conn, $query);
+            echo "Successfully updated";
+            // $resultapp=mysqli_query($conn,"SELECT * FROM judge WHERE judge_id='$username' AND flageval2='N'");
+            // $rowapp = $resultapp->fetch_assoc();
+            // $app_id=$rowapp['app_id'];
+            // $_SESSION['app_id']=$app_id;
+        }
+    }
+    else
+    {
+        echo "Server Error";
+    }
 }
 ?>
